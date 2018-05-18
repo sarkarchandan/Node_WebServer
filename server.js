e5b7fe0428df3d10bd25963d00ead83c4d6d0b9b
chandan <elitegreen@gmail.com>
2018-05-18 23:50:46 +0200

    Initial commit for server project

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+const app = express()
+
+app.set("view engine", hbs);
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper("upperCased", (text) => {
+  return text.toUpperCase();
+});
+
+app.use((request, response, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${request.method} : ${request.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", (error) => {
+    if(error) {
+      console.log("Unable to append server log.");
+    }
+  });
+  next();
+});
+
+app.use((request, response, next) => {
+  response.render("maintainance.hbs");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/home", (request, response) => {
+  response.render("home.hbs", {
+    name: "Chandan",
+    message: "Welcome Home"
+  });
+});
+
+app.get("/about", (request, response) => {
+  response.render("about.hbs", {
+    name: "Chandan",
+    message: "About Chandan"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening to port 3000");
+});
